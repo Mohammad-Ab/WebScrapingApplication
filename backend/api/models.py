9c2b44d42e5533b1e66ac7a065d43c76ce5adfd3
Mohammad-Ab <root.alpha1@gmail.com>
2024-05-20 12:34:57 +0330

    Create Models and Edit Admin.py File

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.conf import settings
+from django.contrib import admin
 
 # Create your models here.
+
+class Corporation(models.Model):
+    title = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=255, unique=True)
+    industry_type = models.TextField(blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+class EsgScore(models.Model):
+    rank = models.IntegerField()
+    total_corporations = models.IntegerField(validators=[MinValueValidator(0)])
+    esg_score = models.IntegerField()
+    environment_pillar = models.IntegerField()
+    social_pillar = models.IntegerField()
+    governance_pillar = models.IntegerField()
+    corporation = models.OneToOneField(Corporation, on_delete=models.CASCADE, primary_key=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.corporation
+    
+    class Meta:
+        ordering = ['esg_score','rank']
+        permissions = [
+                    ('rest_scores', 'can reset all score')
+                ]
+
+
---
+++ backend/api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Corporation,EsgScore
+
+class EsgScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EsgScore
+        exclude =['total_corporations']
+
+    corporation = serializers.StringRelatedField()
+    rank = serializers.SerializerMethodField(method_name='rank_out_of')
+
+    def rank_out_of(self,esgscore:EsgScore):
+        return f'{esgscore.rank}/{esgscore.total_corporations}'
+
+class CorporationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Corporation
+        fields = ['id','title','ticker','industry_type','esgscore']
+
+    esgscore = EsgScoreSerializer()
+
+
